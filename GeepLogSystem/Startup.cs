82e57ba0b4da6a48b575d0a25964167497496e28
冯晓强 <zxcasd555888@163.com>
2015-09-18 11:03:02 +0800

    first commit

---
+++ GeepLogSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeepLogSystem.Startup))]
+namespace GeepLogSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
