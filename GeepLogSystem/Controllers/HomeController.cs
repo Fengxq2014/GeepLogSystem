35b449a3dec527f1c5e4fe014205733fefd517d5
冯晓强 <zxcasd555888@163.com>
2015-10-17 10:10:38 +0800

    20151017

--- GeepLogSystem/Controllers/HomeController.cs
+++ GeepLogSystem/Controllers/HomeController.cs
@@ -8,23 +8,16 @@ namespace GeepLogSystem.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /Home/
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Echarts()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+	}
 }
